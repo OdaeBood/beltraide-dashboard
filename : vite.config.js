e5843b307600315c2f42afe64414b9a5158b4481
OdaeBood <odaerbood@gmail.com>
2025-09-17 15:40:51 +0200

    Update : vite.config.js

--- : vite.config.js
+++ : vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/beltraide-dashboard/',
+  base: '/beltraide-dashboard/', // repo name
 })
